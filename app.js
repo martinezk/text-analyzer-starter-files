588a199232372a80527319113ea3f737a66b5722
Kristina Martinez <misskristinamartinez@gmail.com>
2016-11-22 21:10:09 -0500

    beginning project pseudocode

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-// your code here!
+//Total word count of the submitted text
+//Unique word count of the submitted text
+//Average word length in characters of the submitted text
+//Average sentence length in characters of the submitted text.
+
+$(document).ready(function(){	
+	//add click event to button
+	$('button').on('click', function(event){
+		//define a variable to store what was typed into the field
+		var userText = $(event.currentTarget).prev('#user-text');
+		//set the variable to become an array
+		$(userText).value;
+		//TOTAL WORD COUNT --count the total number of words in the array and show the number
+		var arrayLength = userText.length;
+		console.log(arrayLength);
+		//UNIQUE WORDS ---count how many unique words there are
+		//first, make a loop that looks at each word and only counts the first instance
+		//then show that number
+		var uniqueWords = function(){
+			for (i=userText[0]; i<userText.length; i++){
+				var totalUniqueWds = 0
+				if (i === userText[0]){  //userText[0] is incorrect, needs to loop over the array)
+					totalUniqueWds+=1;
+				}else{
+						continue;
+					}
+				console.log(totalUniqueWds);
+				}
+			}
+		})
+		//AVERAGE WORD LENGTH --splice the original array to find out how many letters there are, ignore spaces
+		//divide the number of letters by the variable arrayLength to get the average word length
+		
+		
+		//AVERAGE SENTENCE LENGTH --splice the array using periods as the breakpoints
+		//then add up the amount of words within the sentences using spaces as break points
+	});
+
